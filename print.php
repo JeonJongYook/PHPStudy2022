b5a4db58591b18dd1f84f22ce90ae8a81612538e
JeonJongYook <jggen0121@naver.com>
2022-08-23 11:20:24 +0900

    2022-08-23_First Update

---
+++ print.php
@@ -0,0 +1,11 @@
+<?php
+    print "문자열 출력<br />";
+    print ("(문자열 출력)<br />");
+    print 10;
+    print "<br />";
+    $str = "변수값 출력 <br />";
+    print $str;
+    print ($str);
+    print 5 + 10;
+    print "<br />";
+?>
