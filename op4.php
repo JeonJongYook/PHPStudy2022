65b642d41031ab2213780590509b408450648a9d
JeonJongYook <jggen0121@naver.com>
2022-08-23 15:17:14 +0900

    2022-08-23_Update

---
+++ op4.php
@@ -0,0 +1,14 @@
+<?php
+    $a = array(1,2);
+    $b = array(3,4,5,6);
+
+    $c = $a + $b;
+    echo "c[0]=", $c[0];
+    echo "<br/>";
+    echo "c[0]=", $c[1];
+    echo "<br/>";
+    echo "c[0]=", $c[2];
+    echo "<br/>";
+    echo "c[0]=", $c[3];
+    echo "<br/>";
+?>
---
+++ str.php
@@ -0,0 +1,7 @@
+<?php
+    $str = "Hello"."word";
+    
+    echo "$str <br />";
+    $str .= " PHP";
+    echo "$str <br />";
+?>  
