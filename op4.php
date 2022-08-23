a696421cc89601481e95c95d43610c6e28e64bcf
JeonJongYook <jggen0121@naver.com>
2022-08-23 15:25:06 +0900

    2022-08-23_Update

--- op4.php
+++ op4.php
@@ -5,10 +5,10 @@
     $c = $a + $b;
     echo "c[0]=", $c[0];
     echo "<br/>";
-    echo "c[0]=", $c[1];
+    echo "c[1]=", $c[1];
     echo "<br/>";
-    echo "c[0]=", $c[2];
+    echo "c[2]=", $c[2];
     echo "<br/>";
-    echo "c[0]=", $c[3];
+    echo "c[3]=", $c[3];
     echo "<br/>";
 ?>
